96e2ef7457aa2b5d5e9259d89fe3b3df50a1e456
mkid <luispalominopaucar@hotmail.com>
2021-09-27 00:28:45 -0500

    Creación del archivo consultas docentes vacio

---
+++ Consultas_Docentes.sql
@@ -0,0 +1,11 @@
+/*
+Autor: Luis Mikhail Palomino Paucar
+Fecha: 01/09/21
+Vistas y procedimientos almacenados para docentes
+TODO: implementar vistas, procedimientos almacenados etc
+*/
+USE master
+go
+
+use DBMATRICULA_DS
+go
