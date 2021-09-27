256c026c6effb35ea8a7d8cc8b86f3318bf6e77b
mkid <luispalominopaucar@hotmail.com>
2021-09-27 08:49:50 -0500

    creacion de vista para la tabla docentes y procedimiento almacenado de clasificacion por genero

--- Consultas_Docentes.sql
+++ Consultas_Docentes.sql
@@ -9,3 +9,87 @@ go
 
 use DBMATRICULA_DS
 go
+
+--tablero docente
+IF OBJECT_ID('vDocente') is not NULL
+    drop VIEW vDocente
+GO
+
+Create view vDocente
+as
+    Select CodDocente, Nombres as Docente, IndSexo as Sexo,Year(FecNacimiento) as YearNacimiento,
+	(Year(GetDate())-Year(FecNacimiento)) as Edad, Especialidad,
+	Nacionalidad,CodDepAcademico,NomDepAcademico from TDocente
+GO
+
+SELECT * from vDocente
+go
+--1. Por sexo
+Select Sexo, count(*) as Cantidad
+from vDocente
+group by Sexo
+---2 Por Departamentos Academicos nro de mujeres y varones
+IF OBJECT_ID('usp_TableroDocentes') is not NULL
+    DROP PROCEDURE usp_TableroDocentes
+GO
+
+create procedure usp_TableroDocentes
+as
+begin
+    BEGIN TRANSACTION Trans
+    BEGIN TRY
+    Declare @DepAcademico varchar(5)
+    Declare @NomDepAcademico varchar(100)
+    Declare @NroVarones int
+    Declare @NroMujeres int
+    Declare @i int
+    Declare @nroRegistros int
+    declare @table table
+    (
+    DepAcademico varchar(5)
+    )
+    insert into @table 
+    select distinct CodDepAcademico from vDocente
+    set @NroRegistros = (select count(*) from @table)
+    declare @Tablero table
+        (
+            Nro int,
+            DepartamentoAcademico varchar(100),
+            NroMujeres int,
+            NroVarones int
+                
+        )
+    set @i=1
+    while (@i<=@nroRegistros)
+    Begin
+        set @DepAcademico = (select max(DepAcademico) from @table)
+        set @NomDepAcademico=(select distinct nomDepAcademico from vDocente where CodDepAcademico=@DepAcademico)
+        set @NroMujeres=(select count(*) from vDocente where Sexo='F' 
+                    and CodDepAcademico=@DepAcademico)
+        set @NroVarones=(select count(*) from vDocente where Sexo='M' 
+                and CodDepAcademico=@DepAcademico)
+    --- insertamos datos del primer rango
+        insert into @Tablero 
+        select @i,@NomDepAcademico,Isnull(@NroMujeres,0), Isnull(@NroVarones,0)
+        ---calculamos los datos del segundo rango
+    set @NroMujeres=(select count(*) from vDocente where Sexo='F' 
+                    and Edad>40 and CodDepAcademico=@DepAcademico)
+        set @NroVarones=(select count(*) from vDocente where Sexo='M' 
+                    and Edad>40 and CodDepAcademico=@DepAcademico)
+        delete from @table where DepAcademico=@DepAcademico
+        set @i=@i+1
+    end--fin de while
+    --calculamos el promedio de mujeres y varones entre 18 y 23 a�os
+        select * from @Tablero
+    COMMIT TRANSACTION Trans
+        SELECT CodError = 0, Mensaje = 'Transacción ejecutada correctamente'
+    END TRY
+    BEGIN CATCH
+        ROLLBACK TRANSACTION Trans
+        SELECT CodError = 1, Mensaje = 'Error, la transacción no se ejecutó'
+    END CATCH
+End--- final
+go 
+
+EXECUTE usp_TableroDocentes
+go
